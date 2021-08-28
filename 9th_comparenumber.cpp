6c66916b1bcd9bc13d5de3957537d10be68118f3
RAHUL MUKATI <87256336+rahulmukati67@users.noreply.github.com>
2021-08-28 20:44:06 +0530

    Create 9th_comparenumber.cpp

---
+++ 9th_comparenumber.cpp
@@ -0,0 +1,23 @@
+/* 
+NAME : RAHUL MUKATI
+ENO :  0827CI201150
+CSIT -3 
+program - 8th :“Program for input two number and check whether it is Equal or not”*/
+
+#include<iostream>
+using namespace std;
+int main()
+{  
+
+    int num1, num2;
+    cout<<"Enter two number to compare : ";
+    cin>>num1>>num2;
+    if(num1==num2)
+    {
+        cout<<"Given numbers are equal  ";
+    }
+    else 
+    cout<<"Given numbers not are equal  ";
+
+    return 0 ;
+}
