4835b7a8b94589bd79878258a4df8ebdd52463cc
RAHUL MUKATI <87256336+rahulmukati67@users.noreply.github.com>
2021-08-28 20:40:08 +0530

    Create 8th_rectangle.cpp

---
+++ 8th_rectangle.cpp
@@ -0,0 +1,28 @@
+/* 
+NAME : RAHUL MUKATI
+ENO :  0827CI201150
+CSIT -3 
+program - 8th :“Program for Calculating area, Diagonal and Perimeter of Rectangle”*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float l, b, h;
+    cout << "Enter length of Rectangle : ";
+    cin >> l;
+    cout << endl;
+    cout << "Enter breath of Rectangle : ";
+    cin >> b;
+    cout << endl;
+    cout << "Enter height of Rectangle : ";
+    cin >> h;
+    cout << endl;
+
+    cout << "Area of rectangle : " << l * b * h << endl;
+    cout << "Perimeter of rectangle : " << 2 * (l + h) << endl;
+    cout << "Diagonal of rectangle : " << sqrt(pow(l, 2) + pow(b, 2)) << endl;
+
+    return 0;
+}
