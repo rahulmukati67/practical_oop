d96d9f6adda331a43c2deb970ecf831b1b1ee838
RAHUL MUKATI <87256336+rahulmukati67@users.noreply.github.com>
2021-08-30 11:10:05 +0530

    Create _19_loop.cpp

---
+++ _19_loop.cpp
@@ -0,0 +1,17 @@
+/* 
+NAME : RAHUL MUKATI
+ENO :  0827CI201150
+CSIT -3 
+program -20 “Program using for loop to print numbers from 1 to 10” */
+
+#include<iostream>
+using namespace std ;
+
+int main()
+{   int i,a[10]={1,2,3,4,5,6,7,8,9,10};
+    for (i=0;i<10;i++)
+    { 
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}
